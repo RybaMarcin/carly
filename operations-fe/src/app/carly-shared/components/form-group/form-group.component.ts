cccc1184418cefe611a8e484f750deb8e264595e
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2019-10-08 10:30:27 +0200

    Implement components for form-group.

---
+++ operations-fe/src/app/carly-shared/components/form-group/form-group.component.ts
@@ -0,0 +1,33 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {FormGroup} from "@angular/forms";
+import {FormGroupHelper} from "../../model/form-group-helper.model";
+
+@Component({
+  selector: 'form-group',
+  templateUrl: './form-group.component.html',
+  styleUrls: ['./form-group.component.scss']
+})
+export class FormGroupComponent {
+
+  @Input() fg: FormGroup;
+  @Input() gridColumns?: number = 4;
+  @Input() name: string;
+  @Input() controls: FormGroupHelper.ModelControl;
+  @Output() click = new EventEmitter<string>();
+
+  checkForErrors(fgName: string, fcName: string, error: FormGroupHelper.ErrorValidators, fcNestedName?: string) {
+    const formGroup = this.fg.get(fgName);
+    const formNestedControl = fcNestedName? formGroup.get(fcNestedName) : formGroup;
+    const formControl = formNestedControl.get(fcName);
+
+    return formControl.errors && formControl.errors[error.validator];
+  }
+
+  onClick(element: string) {
+    this.click.emit(element);
+    console.log(element);
+  }
+
+
+
+}
