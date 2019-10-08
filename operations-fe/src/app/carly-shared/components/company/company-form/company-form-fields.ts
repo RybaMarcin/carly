9668813183d84787f65bd4a87479e6c4a7c22a9b
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2019-10-08 10:37:52 +0200

    Add components for add, edit and accept company.

---
+++ operations-fe/src/app/carly-shared/components/company/company-form/company-form-fields.ts
@@ -0,0 +1,115 @@
+import {FormGroupHelper} from "../../../model/form-group-helper.model";
+import {ValueLabel} from "../../../model/value-label";
+import {Validators} from "@angular/forms";
+
+
+const temporaryUserEmails = function(): Array<ValueLabel> {
+
+  const emails = [
+      'carly-user@gmail.com',
+      'carly-user2@gmail.com',
+      'carly-user3@gmail.com',
+      'carly-user4@gmail.com',
+      'carly-user5@gmail.com',
+      'carly-user6@gmail.com',
+      'carly-user7@gmail.com',
+      'carly-user8@gmail.com',
+      'carly-user9@gmail.com',
+      'carly-user10@gmail.com',
+      'carly-user11@gmail.com',
+      'carly-user12@gmail.com',
+      'carly-user13@gmail.com',
+      'carly-user14@gmail.com',
+      'carly-user15@gmail.com',
+  ];
+
+  return emails.map(email => ({label: email, value: email}));
+
+};
+
+
+export const companyDetailsFormFields: FormGroupHelper.Model[] = [
+    {
+      inputName: 'name',
+      label: 'Company name',
+      validators: [Validators.required],
+      type: 'text',
+      rows: 1,
+      cols: 1,
+    },
+    {
+      inputName: 'brand',
+      label: 'Brand name',
+      validators: [Validators.required],
+      type: 'text',
+      rows: 1,
+      cols: 1,
+    },
+    {
+      inputName: 'number',
+      label: 'Company number',
+      validators: [Validators.required],
+      type: 'text',
+      rows: 1,
+      cols: 1,
+    },
+    {
+      inputName: 'email',
+      label: 'Email address',
+      validators: [Validators.required],
+      type: 'text',
+      rows: 1,
+      cols: 1,
+    },
+    {
+      inputName: 'phoneNumber',
+      label: 'Phone number',
+      validators: [Validators.required],
+      type: 'text',
+      rows: 1,
+      cols: 1,
+    }
+];
+
+export const addressDetailsFormFields: FormGroupHelper.Model[] = [
+    {
+      inputName: 'city',
+      label: 'City',
+      validators: [Validators.required],
+      type: 'text',
+      rows: 1,
+      cols: 1
+    },
+    {
+      inputName: 'country',
+      label: 'Country',
+      validators: [Validators.required],
+      type: 'text',
+      rows: 1,
+      cols: 1
+    },
+    {
+      inputName: 'firstLine',
+      label: 'First line',
+      validators: [Validators.required],
+      type: 'text',
+      rows: 1,
+      cols: 1
+    },
+    {
+      inputName: 'secondLine',
+      label: 'Second line',
+      validators: [Validators.required],
+      type: 'text',
+      rows: 1,
+      cols: 1
+    },
+    {
+      inputName: 'zipCode',
+      label: 'Zip code',
+      validators: [Validators.required],
+      type: 'text',
+      rows: 1,
+      cols: 1
+    },
+];
