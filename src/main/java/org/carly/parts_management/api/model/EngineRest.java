8712aaf683c35c608f51ae8299bc6319c6ef7522
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2019-10-21 21:44:36 +0200

    Implement EngineMapper.

--- src/main/java/org/carly/parts_management/api/model/EngineRest.java
+++ src/main/java/org/carly/parts_management/api/model/EngineRest.java
@@ -1,4 +1,20 @@
 package org.carly.parts_management.api.model;
 
+import lombok.Data;
+import org.bson.types.ObjectId;
+
+import java.math.BigDecimal;
+
+@Data
 public class EngineRest {
+
+    private ObjectId id;
+    private String name;
+    private BigDecimal price;
+    private BrandRest brand;
+    private double horsePower;
+    private double weight;
+    private double capacity;
+    private int numberOfCylinders;
+
 }
