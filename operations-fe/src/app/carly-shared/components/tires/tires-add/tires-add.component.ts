5a20e86ccf9319a3a251d260213b0eb2a02fecfd
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2019-10-26 17:57:32 +0200

    Implement components for add and edit tires.

---
+++ operations-fe/src/app/carly-shared/components/tires/tires-add/tires-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tires-add',
+  templateUrl: './tires-add.component.html',
+  styleUrls: ['./tires-add.component.scss']
+})
+export class TiresAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
