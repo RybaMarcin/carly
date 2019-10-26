746688c4d5e67cf71ba858bc452c79f6131146fe
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2019-10-26 18:00:24 +0200

    Implement painting add and edit components.

---
+++ operations-fe/src/app/carly-shared/components/painting/painting-add/painting-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'painting-add',
+  templateUrl: './painting-add.component.html',
+  styleUrls: ['./painting-add.component.scss']
+})
+export class PaintingAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
