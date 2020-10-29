eac9b7ef20c487f04770c6d233501a1e7745f904
mryba <ryba.marcin20@gmail.com>
2020-10-29 15:45:30 +0100

    Change role to ADMINISTRATOR + add authorize in breaks controller

--- src/main/java/org/carly/api/endpoint/BreaksController.java
+++ src/main/java/org/carly/api/endpoint/BreaksController.java
@@ -7,8 +7,10 @@ import org.carly.api.rest.partsmanagement.BreaksRest;
 import org.carly.api.rest.partsmanagement.criteria.BreaksSearchCriteriaRest;
 import org.carly.core.partsmanagement.service.BreaksFindService;
 import org.carly.core.partsmanagement.service.BreaksSaveService;
+import org.carly.core.security.service.LoggedUserProvider;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
@@ -20,40 +22,45 @@ public class BreaksController {
 
     private final BreaksFindService breaksFindService;
     private final BreaksSaveService breaksSaveService;
-
+    private final LoggedUserProvider loggedUserProvider;
 
     public BreaksController(BreaksFindService breaksFindService,
-                            BreaksSaveService breaksSaveService) {
+                            BreaksSaveService breaksSaveService,
+                            LoggedUserProvider loggedUserProvider) {
         this.breaksFindService = breaksFindService;
         this.breaksSaveService = breaksSaveService;
+        this.loggedUserProvider = loggedUserProvider;
     }
 
     @GetMapping("/breaks")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRATOR','CARLY_COMPANY', 'CARLY_CUSTOMER')")
     public Page<BreaksRest> findBreaks(BreaksSearchCriteriaRest searchCriteria, Pageable pageable) {
         return breaksFindService.findBreaks(searchCriteria, pageable);
     }
 
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRATOR','CARLY_COMPANY', 'CARLY_CUSTOMER')")
     public BreaksRest findBreaksById(@PathVariable("id") String id) {
         return breaksFindService.findPartById(new ObjectId(id));
     }
 
 
     @GetMapping("/all")
+    @PreAuthorize("hasAnyAuthority('ADMINISTRATOR','CARLY_COMPANY', 'CARLY_CUSTOMER')")
     public Collection<BreaksRest> findAllBreaks() {
         return breaksFindService.findAll();
     }
 
     @PostMapping()
+    @PreAuthorize("hasAnyAuthority('ADMINISTRATOR','CARLY_COMPANY')")
     public BreaksRest createBreaks(@RequestBody BreaksRest breaks) {
         return breaksSaveService.createPart(breaks);
     }
 
     @PutMapping
+    @PreAuthorize("hasAnyAuthority('ADMINISTRATOR','CARLY_COMPANY')")
     public BreaksRest updateBreaks(@RequestBody BreaksRest breaks) {
         return breaksSaveService.updatePart(breaks);
     }
-
-
 }
