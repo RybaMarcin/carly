3623ea26e206deec1c14c858272be9d625457937
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-10 08:49:23 +0100

    Change paths in LoginController.

--- src/main/java/org/carly/api/endpoint/LoginController.java
+++ src/main/java/org/carly/api/endpoint/LoginController.java
@@ -2,6 +2,7 @@ package org.carly.api.endpoint;
 
 import org.carly.api.rest.request.LoginRequest;
 import org.carly.api.rest.request.SignupRequest;
+import org.carly.api.rest.response.MessageResponse;
 import org.carly.core.usermanagement.service.TokenBlackListService;
 import org.carly.core.usermanagement.service.UserService;
 import org.springframework.http.HttpHeaders;
@@ -30,17 +31,22 @@ public class LoginController {
         return userService.login(loginRequest);
     }
 
-    @PostMapping("/signup")
-    public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest, WebRequest webRequest) {
-        return userService.register(signUpRequest, webRequest);
+    @PostMapping("/signup-customer")
+    public ResponseEntity<?> registerCustomer(@Valid @RequestBody SignupRequest signUpRequest, WebRequest webRequest) {
+        return userService.register(signUpRequest, webRequest, false);
     }
 
-    @GetMapping("/refreshToken")
-    public ResponseEntity<String> refreshToken(@RequestHeader(value = HttpHeaders.AUTHORIZATION) String bearerToken) {
+    @PostMapping("/signup-company")
+    public ResponseEntity<?> registerCompany(@Valid @RequestBody SignupRequest signUpRequest, WebRequest webRequest) {
+        return userService.register(signUpRequest, webRequest, true);
+    }
+
+    @GetMapping("/refresh-token")
+    public ResponseEntity<?> refreshToken(@RequestHeader(value = HttpHeaders.AUTHORIZATION) String bearerToken) {
         return userService.refreshToken(bearerToken);
     }
 
-    @GetMapping("/registrationConfirmation")
+    @GetMapping("/registration-confirmation")
     public String confirmRegistration(WebRequest request,
                                       @RequestParam("token") String token) {
         String response = userService.confirmRegistration(request, token);
@@ -50,6 +56,6 @@ public class LoginController {
     @GetMapping("/logout")
     public ResponseEntity<?> logout(@RequestHeader(value = HttpHeaders.AUTHORIZATION) String bearerToken) {
         blackListService.addTokenToBlackList(bearerToken);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new MessageResponse("Logout successfully!"));
     }
 }
