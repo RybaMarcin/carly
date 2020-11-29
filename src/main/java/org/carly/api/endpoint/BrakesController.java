eb154ab747c71486bbb005f48177f33237dc04ff
Damian Szwajkos <damian.szwajkos@zalassconsulting.pl>
2020-11-29 11:34:26 +0100

    Rename brake controller, services and repository.

--- src/main/java/org/carly/api/endpoint/BrakesController.java
+++ src/main/java/org/carly/api/endpoint/BrakesController.java
@@ -7,7 +7,7 @@ import org.carly.api.rest.criteria.BreaksSearchCriteriaRequest;
 import org.carly.api.rest.request.BrakeRequest;
 import org.carly.api.rest.response.BrakeResponse;
 import org.carly.api.rest.response.SuccessResponse;
-import org.carly.core.partsmanagement.service.BreaksFindService;
+import org.carly.core.partsmanagement.service.BrakeFindService;
 import org.carly.core.partsmanagement.service.BrakeSaveService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,53 +18,53 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/api//breaks")
-public class BreaksController {
+@RequestMapping("/api/brakes")
+public class BrakesController {
 
-    private static final String DELETE_BREAKS = "Delete breaks by id";
-    private final BreaksFindService breaksFindService;
+    private static final String DELETE_BRAKES = "Delete brakes by id";
+    private final BrakeFindService brakeFindService;
     private final BrakeSaveService brakeSaveService;
 
-    public BreaksController(BreaksFindService breaksFindService,
+    public BrakesController(BrakeFindService brakeFindService,
                             BrakeSaveService brakeSaveService) {
-        this.breaksFindService = breaksFindService;
+        this.brakeFindService = brakeFindService;
         this.brakeSaveService = brakeSaveService;
     }
 
-    @GetMapping("/breaks")
+    @GetMapping("/brakes")
     @PreAuthorize("hasAnyAuthority('ADMINISTRATOR', 'CARLY_FACTORY', 'CARLY_COMPANY', 'CARLY_CUSTOMER')")
-    public Page<BrakeResponse> findBreaks(BreaksSearchCriteriaRequest searchCriteria, Pageable pageable) {
-        return breaksFindService.findBreaks(searchCriteria, pageable);
+    public Page<BrakeResponse> findBrakes(BreaksSearchCriteriaRequest searchCriteria, Pageable pageable) {
+        return brakeFindService.findBreaks(searchCriteria, pageable);
     }
 
     @GetMapping("/{id}")
     @PreAuthorize("hasAnyAuthority('ADMINISTRATOR','CARLY_FACTORY', 'CARLY_COMPANY', 'CARLY_CUSTOMER')")
-    public BrakeResponse findBreaksById(@PathVariable("id") String id) {
-        return breaksFindService.findPartById(new ObjectId(id));
+    public BrakeResponse findBrakeById(@PathVariable("id") String id) {
+        return brakeFindService.findPartById(new ObjectId(id));
     }
 
     @GetMapping("/all")
     @PreAuthorize("hasAnyAuthority('ADMINISTRATOR', 'CARLY_FACTORY', 'CARLY_COMPANY', 'CARLY_CUSTOMER')")
-    public Collection<BrakeResponse> findAllBreaks() {
-        return breaksFindService.findAll();
+    public Collection<BrakeResponse> findAllBrakes() {
+        return brakeFindService.findAll();
     }
 
     @PostMapping()
     @PreAuthorize("hasAnyAuthority('ADMINISTRATOR','CARLY_FACTORY')")
-    public ResponseEntity<BrakeResponse> createBreaks(@RequestBody BrakeRequest brakeRequest) {
+    public ResponseEntity<BrakeResponse> createBrake(@RequestBody BrakeRequest brakeRequest) {
         return brakeSaveService.createPart(brakeRequest);
     }
 
     @PutMapping
     @PreAuthorize("hasAnyAuthority('ADMINISTRATOR','CARLY_FACTORY')")
-    public ResponseEntity<BrakeResponse> updateBreaks(@RequestBody BrakeRequest breaks) {
+    public ResponseEntity<BrakeResponse> updateBreak(@RequestBody BrakeRequest breaks) {
         return brakeSaveService.updatePart(breaks);
     }
 
-    @DeleteMapping("/delete/{breakId}")
+    @DeleteMapping("/delete/{brakeId}")
     @PreAuthorize("hasAnyAuthority('ADMINISTRATOR', 'CARLY_FACTORY')")
-    @ApiOperation(value = DELETE_BREAKS)
-    public ResponseEntity<SuccessResponse> deleteBreaks(@PathVariable(name = "breakId") String breakId) {
-        return brakeSaveService.deleteBreak(breakId);
+    @ApiOperation(value = DELETE_BRAKES)
+    public ResponseEntity<SuccessResponse> deleteBrake(@PathVariable(name = "brakeId") String brakeId) {
+        return brakeSaveService.deleteBrake(brakeId);
     }
 }
--- src/main/java/org/carly/core/partsmanagement/service/BrakeFindService.java
+++ src/main/java/org/carly/core/partsmanagement/service/BrakeFindService.java
@@ -5,7 +5,7 @@ import org.bson.types.ObjectId;
 import org.carly.api.rest.criteria.BreaksSearchCriteriaRequest;
 import org.carly.api.rest.response.BrakeResponse;
 import org.carly.core.partsmanagement.mapper.BrakeResponseMapper;
-import org.carly.core.partsmanagement.repository.BreaksMongoRepository;
+import org.carly.core.partsmanagement.repository.BrakeMongoRepository;
 import org.carly.core.partsmanagement.repository.BrakeRepository;
 import org.carly.core.shared.exception.EntityNotFoundException;
 import org.carly.core.shared.service.part_services.PartFindService;
@@ -22,33 +22,33 @@ import static org.carly.core.shared.utils.InfoUtils.NOT_FOUND;
 
 @Service
 @Slf4j
-public class BreaksFindService implements PartFindService {
+public class BrakeFindService implements PartFindService {
 
 
     private final BrakeResponseMapper brakeResponseMapper;
-    private final BrakeRepository breaksrepository;
-    private final BreaksMongoRepository breaksMongoRepository;
+    private final BrakeRepository brakesRepository;
+    private final BrakeMongoRepository brakeMongoRepository;
 
 
-    public BreaksFindService(BrakeResponseMapper brakeResponseMapper,
-                             BrakeRepository brakeRepository,
-                             BreaksMongoRepository breaksMongoRepository) {
+    public BrakeFindService(BrakeResponseMapper brakeResponseMapper,
+                            BrakeRepository brakeRepository,
+                            BrakeMongoRepository brakeMongoRepository) {
         this.brakeResponseMapper = brakeResponseMapper;
-        this.breaksrepository = brakeRepository;
-        this.breaksMongoRepository = breaksMongoRepository;
+        this.brakesRepository = brakeRepository;
+        this.brakeMongoRepository = brakeMongoRepository;
     }
 
 
     @Override
     public Collection<BrakeResponse> findAll() {
-        List<Brake> brakeList = breaksrepository.findAll();
+        List<Brake> brakeList = brakesRepository.findAll();
         log.info("Breaks list contains: {}", brakeList.size());
         return brakeList.stream().map(brakeResponseMapper::simplifyRestObject).collect(Collectors.toList());
     }
 
     @Override
     public BrakeResponse findPartById(ObjectId id) {
-        Brake aBrake = breaksrepository.findById(id)
+        Brake aBrake = brakesRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException(NOT_FOUND));
         log.info("Breaks with id: {} was found!", id);
         return brakeResponseMapper.simplifyRestObject(aBrake);
@@ -56,7 +56,7 @@ public class BreaksFindService implements PartFindService {
 
 
     public Page<BrakeResponse> findBreaks(BreaksSearchCriteriaRequest searchCriteria, Pageable pageable) {
-        return breaksMongoRepository.findWithFilters(searchCriteria, pageable)
+        return brakeMongoRepository.findWithFilters(searchCriteria, pageable)
                 .map(brakeResponseMapper::simplifyRestObject);
     }
 
